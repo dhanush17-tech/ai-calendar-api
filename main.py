b6121e89ed0f1f197d94442a16bdc6f2862a70cb
Dhanush Vardhan <dhanush.kalaiselvan@gmail.com>
2024-02-22 14:23:17 -0700

    check

--- main.py
+++ main.py
@@ -360,4 +360,4 @@ def editGoogleCalendarEvent(accessToken: str, refreshToken: str, jsonData, timez
     return jsonData["message"]
 
 if __name__ == "__main__":
-    app.run(port=os.getenv("PORT", default=8000), debug=True)
+    app.run(port=os.getenv("PORT", default=8000), host="0.0.0.0", debug=True)
